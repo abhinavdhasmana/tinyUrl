030497acfefda9e8e6fd973ab3602bf00819d07f
abhinavdhasmana <abhinav.dhasmana@live.com>
2018-03-06 13:02:45 +0530

    Adding seeders

---
+++ seeders/20180306055750-seedUrls.js
@@ -0,0 +1,46 @@
+/* eslint-disable */
+const helper = require('../src/helpers/urlShortner');
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    const allData = [];
+    const allCodes = {};
+    for (let i = 0; i < 1000000; i += 1) {
+      const code = helper.generateShortURL(`url${i}`, 0, 5);
+      if (!allCodes[code]) {
+        allCodes[code] = true;
+        const temp = {
+          code,
+          originalUrl: `url${i}`,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        };
+        allData.push(temp);
+      }
+    }
+    // console.log(Sequelize.Model);
+    // return Sequelize.Model.urls.bulkCreate(allData);
+    console.log(allData.length);
+    return queryInterface.bulkInsert('urls', allData);
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('Person', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('Person', null, {});
+    */
+  },
+};
