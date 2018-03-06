7b0f57093b4fb1d7a06dc49e51a2fe2b328429c5
abhinavdhasmana <abhinav.dhasmana@live.com>
2018-03-06 14:13:28 +0530

    Adding good console

--- src/server.js
+++ src/server.js
@@ -1,9 +1,33 @@
 const Hapi = require('hapi');
 const Routes = require('./routes');
+const Good = require('good');
+
 
 const server = new Hapi.Server();
 server.connection({ port: 3000, host: 'localhost' });
 server.route(Routes);
+
+server.register({
+  register: Good,
+  options: {
+    reporters: {
+      console: [{
+        module: 'good-squeeze',
+        name: 'Squeeze',
+        args: [{
+          response: '*',
+          log: '*',
+        }],
+      }, {
+        module: 'good-console',
+      }, 'stdout'],
+    },
+  },
+}, (err) => {
+  if (err) {
+    throw err;
+  }
+});
 
 if (!module.parent) {
   server.start((err) => {
