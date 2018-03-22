04e412b7b2e64b91b3731292b51ee1df4ea4aa8c
abhinavdhasmana <abhinav.dhasmana@live.com>
2018-03-22 11:14:26 +0530

    Updating the script for ec2 user data

--- src/start.sh
+++ src/start.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
-sudo apt update
-sudo apt install htop
+apt update
+apt install htop
+cat > /tmp/subscript.sh << EOF
+# START UBUNTU USERSPACE
+echo "Setting up NodeJS Environment"
+curl https://raw.githubusercontent.com/creationix/nvm/v0.25.0/install.sh | bash
+echo 'export NVM_DIR="/home/ubuntu/.nvm"' >> /home/ubuntu/.bashrc
+echo '[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm' >> /home/ubuntu/.bashrc
+# Dot source the files to ensure that variables are available within the current shell
+. /home/ubuntu/.nvm/nvm.sh
+. /home/ubuntu/.profile
+. /home/ubuntu/.bashrc
+# Install NVM, NPM, Node.JS & Grunt
+cd /home/ubuntu/
 git clone https://github.com/abhinavdhasmana/tinyUrl.git
 cd tinyUrl
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
-export NVM_DIR="/home/ubuntu/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-source /home/ubuntu/.bashrc
 nvm install 8.9.4
 npm install
 npm start
+EOF
+
+chown ubuntu:ubuntu /tmp/subscript.sh && chmod a+x /tmp/subscript.sh
+sleep 1; su - ubuntu -c "/tmp/subscript.sh"
 
